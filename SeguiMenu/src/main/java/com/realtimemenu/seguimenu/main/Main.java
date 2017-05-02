ea70615d265cd8ce99b2885cf67173127db32e8d
Gianluca Stella <gianluca.stella@xtphere.com>
2017-05-02 18:18:36 +0200

    Creato progetto

---
+++ SeguiMenu/src/main/java/com/realtimemenu/seguimenu/main/Main.java
@@ -0,0 +1,14 @@
+package com.realtimemenu.seguimenu.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		logger.info("SeguiMenu started.");
+	}
+
+}
